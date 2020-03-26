19faac39b58f707960fd9716da3548961c682f15
William Qin <wqin2008@gmail.com>
2020-03-26 17:21:26 -0400

    First steps

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+from dotenv import load_dotenv()
+load_dotenv()
+#Note: Use os.getenv() to get values in the dotenv file.
+
+@app.route('/')
+def index():
+    return render_template("StartPage")
